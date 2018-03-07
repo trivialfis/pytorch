78842946d85e008a9c6fd88eff786f2033071ecc
fis <ybbs.daans@hotmail.com>
2018-03-07 21:10:26 +0800

    Add macro for generating types.
    * aten/src/ATen/type_template.h(dummytype): New struct.
    (DEFINE_CL_TYPE, DEFINE_CPU_TYPE, DEFINE_CUDA_TYPE): New macros.

--- aten/src/ATen/type_template.h
+++ aten/src/ATen/type_template.h
@@ -7,10 +7,19 @@
 #include "ATen/Scalar.h"
 // #include "ATen/Tensor.h"
 #include "ATen/Allocator.h"
+#include "ATen/Type.h"
 
 namespace at{
 template <Backend b, ScalarType s>
-AT_API struct dummytype
+struct dummytype final : public Type
 {
+  explicit dummytype(Context* context)
+    : Type(context) {}
 };
+#define DEFINE_CL_TYPE(_1, n, _2)					\
+  using CL ## n ## Type = dummytype<Backend::CL, ScalarType::n>;
+#define DEFINE_CPU_TYPE(_1, n, _2)					\
+  using CPU ## n ## Type = dummytype<Backend::CPU, ScalarType::n>;
+#define DEFINE_CUDA_TYPE(_1, n, _2)					\
+  using CUDA ## n ## Type = dummytype<Backend::CUDA, ScalarType::n>
 }
